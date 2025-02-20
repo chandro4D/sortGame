71dd0b6e38e1e76150d83dd00cf5a2e80a617ce6
chandro4D <chandroshakhor7@gmail.com>
2025-02-20 21:38:39 +0600

    add some new

--- sortGame.py
+++ sortGame.py
@@ -7,6 +7,7 @@ class Tube:
 
     def add_color(self, color):
         self.colors.append(color)
+
     def remove_color(self):
         if self.is_empty():
             return None
@@ -16,24 +17,38 @@ class Tube:
         if self.is_empty():
             return None
         return self.colors[-1]
-        def is_full(self):
+
+    def is_full(self):
         return len(self.colors) == 4
 
     def is_empty(self):
         return len(self.colors) == 0
+
     def can_pour_into(self, other):
         if self.is_empty() or other.is_full():
             return False
         if other.is_empty():
             return True
         return self.peek_color() == other.peek_color()
+
+
 class WaterSortGame:
     def __init__(self, root, num_tubes):
         self.tubes = [Tube() for _ in range(num_tubes)]
         self.root = root
         self.selected_tube = None
+
         self.setup_ui()
 
     def setup_ui(self):
         self.root.configure(bg="#303036")
         self.root.geometry("1650x1000")
+
+        title = tk.Label(
+            self.root,
+            text="WATER SORT PUZZLE",
+            font=("Arial", 40, "bold"),
+            bg="#303036",
+            fg="#FFD700",
+        )
+        title.pack(pady=120)
